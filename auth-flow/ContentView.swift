310d933141ba602133a5ea04da1100862265fe8e
Pramodya <pramodya.emedia@gmail.com>
2021-05-14 12:33:08 +0530

    Initial Commit

---
+++ auth-flow/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  auth-flow
+//
+//  Created by Pramodya Abeysinghe on 2021-05-14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
